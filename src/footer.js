86e457c7c0ec42460a24b65a9bbaf80eb8bdd137
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-01 12:07:55 -0400

    Add my basic footer content to footer module

--- src/footer.js
+++ src/footer.js
@@ -1,8 +1,10 @@
-import Logo from "./bbq-icon.png";
+import githubLogo from "./github-mark.png";
 export { loadFooter };
 
 function loadFooter() {
     const footer = document.querySelector("footer");
     footer.innerHTML = `
+        Copyright © 2023 sourdoughbredd
+        <a href="https://github.com/sourdoughbredd"><img src=${githubLogo} id="github-logo"></a>
     `;
 };
