8295fca9b0232bea900c162485eadb8686b99d72
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-01 11:51:55 -0400

    Implement some tab switching and add header load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import "./style.css";
-import { loadHomePage } from "./home.js";
-import { loadHoursPage } from "./hours.js";
+import { loadHeader } from "./header";
+import { loadHome } from "./home.js";
+import { loadHours } from "./hours.js";
 
-loadHomePage();
+loadHeader();
+loadHome();
+
+const content = document.querySelector("#content");
+const homeBtn = document.querySelector("#home-link");
+const menuBtn = document.querySelector("#menu-link");
+const hoursBtn = document.querySelector("#hours-link");
+const contactBtn = document.querySelector("#contact-link");
+
+homeBtn.addEventListener("click", () => {
+    // Clear main content
+    content.innerHTML = "";
+    // Load home page
+    loadHome();
+});
+
+hoursBtn.addEventListener("click", () => {
+    // Clear main content
+    content.innerHTML = "";
+    // Load hours page
+    loadHours();
+})
