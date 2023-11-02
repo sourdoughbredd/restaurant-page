015f198b0a728731eed50b82404cb0d32b73f5ad
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-02 13:14:09 -0400

    Add image attributions to footer

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,11 @@ export { loadFooter };
 function loadFooter() {
     const footer = document.querySelector("footer");
     footer.innerHTML = `
-        Copyright © 2023 sourdoughbredd
-        <a href="https://github.com/sourdoughbredd"><img src=${githubLogo} id="github-logo"></a>
-    `;
+        <div>
+            Copyright © 2023 sourdoughbredd
+            <a href="https://github.com/sourdoughbredd"><img src=${githubLogo} id="github-logo"></a>
+        </div>
+        <a href="https://www.flaticon.com/free-icons/bbq" title="bbq icons">BBQ icons created by Freepik - Flaticon</a>
+        <a href="https://unsplash.com/@emersonvieira" title="bbq photo">BBQ photo created by Emerson Vieira on Unsplash</a>
+        `;
 };
