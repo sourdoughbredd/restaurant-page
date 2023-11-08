6de1c12f2e2a7ef848f71bb917bb7b7922307a29
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-08 10:42:36 -0500

    Add comments for neatness

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import { loadHome } from "./home";
 import { loadHours } from "./hours";
 import { loadMenu } from "./menu";
 
+// Load home page
 loadHeader();
 loadFooter();
 loadHome();
 
+// Get DOM elements for page navigation
 const content = document.querySelector("#content");
 const homeBtn = document.querySelector("#home-link");
 const menuBtn = document.querySelector("#menu-link");
 const hoursBtn = document.querySelector("#hours-link");
 const contactBtn = document.querySelector("#contact-link");
 
+// Add event listeners to nav buttons
 homeBtn.addEventListener("click", () => {
     // Clear main content
     content.innerHTML = "";
